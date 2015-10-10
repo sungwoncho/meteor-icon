979f8ff366e527d435c543d39a8b71bb65ede979
Matthew Wood <wood_matt@optusnet.com.au>
2015-10-10 23:44:02 +1100

    Changes to Svg and define if no package

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,7 @@ WebApp.connectHandlers.use("/package", function(request, response) {
 ?names=${request.url.split('/')[1]}`;
   console.log(url);
   HTTP.get(url, {headers: {'Accept': 'application/json'}}, function(err, res) {
+    var name = '';
     if (res.data.length != 0) {
       name = res.data[0].name;
       version = res.data[0].latestVersion.version;
@@ -14,7 +15,7 @@ WebApp.connectHandlers.use("/package", function(request, response) {
     }
 
     SSR.compileTemplate('icon', Assets.getText('icon.svg'));
-    var params = {width: 300, n: name, v: version, p: pubdate,
+    var params = {width: 350, n: name, v: version, p: pubdate,
       s: starCount.toLocaleString(), i: installyear.toLocaleString()};
     var icon = SSR.render('icon', params);
 
