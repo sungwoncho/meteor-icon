f04ab37eac01814fe6d8291b4914a4b138ba3062
Sung Won Cho <mikeswcho@gmail.com>
2015-10-10 23:07:49 +1100

    Trim lines with more than 80 characters

--- client/home.js
+++ client/home.js
@@ -4,7 +4,8 @@ Template.home.events({
     var fullName = tpl.$('.package-name').val();
     var iconPath = Meteor.absoluteUrl(`package/${fullName}`);
     tpl.$('.demo-icon').attr('src', iconPath);
-    var embedCode = `[![Meteor Icon](${iconPath})](https://atmospherejs.com/${fullName.split(':')[0]}/${fullName.split(':')[1]}})`
+    var embedCode = `[![Meteor Icon](${iconPath})](https://atmospherejs.com/\
+${fullName.split(':')[0]}/${fullName.split(':')[1]}})`;
     tpl.$('.embed-code').val(embedCode).removeClass('hidden');
   },
   'click .embed-code': function (e, tpl) {
--- server/routes.js
+++ server/routes.js
@@ -1,19 +1,21 @@
-var config = { w: 300 }
+var f = "MMM Do YYYY";
 
 WebApp.connectHandlers.use("/package", function(request, response) {
-  var url = `https://atmospherejs.com/a/packages/findByNames?names=${request.url.split('/')[1]}`;
+  var url = `https://atmospherejs.com/a/packages/findByNames\
+?names=${request.url.split('/')[1]}`;
+  console.log(url);
   HTTP.get(url, {headers: {'Accept': 'application/json'}}, function(err, res) {
     if (res.data.length != 0) {
       name = res.data[0].name;
       version = res.data[0].latestVersion.version;
-      pubdate = moment(res.data[0].latestVersion.published.$date).format("MMM Do YYYY");
+      pubdate = moment(res.data[0].latestVersion.published.$date).format(f);
       starCount = res.data[0].starCount;
       installyear = res.data[0]['installs-per-year'];
     }
 
     SSR.compileTemplate('icon', Assets.getText('icon.svg'));
-    var params = {$wi: config.w, $n: name, $v: version, $p: pubdate,
-      $s: starCount.toLocaleString(), $i: installyear.toLocaleString()};
+    var params = {width: 300, n: name, v: version, p: pubdate,
+      s: starCount.toLocaleString(), i: installyear.toLocaleString()};
     var icon = SSR.render('icon', params);
 
     response.writeHead(200, {"Content-Type": "image/svg+xml"});
