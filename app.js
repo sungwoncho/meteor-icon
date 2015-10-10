472fd173f97cd6f4676dbdbc9ef6ba87541573ab
Tomas Trescak <tomi.trescak@gmail.com>
2015-10-10 16:30:56 +1100

    Stars

--- app.js
+++ app.js
@@ -22,6 +22,26 @@
 //   });
 // }
 
+// var drawStar(x, y, width, height, strokeWidth, fillColor, strokeColor) {
+//   '<polygon points="' +
+//   x + ',' + y + ' ' +
+//   (x + width/2) + ',' + (y + height) + ' ' +
+//   (x + width) + ',' + y + ' ' +
+//   x + ',' + (y + ' ' +
+//   (x + width) + ',' + y + '" ' +
+//   'style="fill:' + fillColor' + ';' +
+//   'stroke:' + strokeColor' + ';' +
+//   'stroke-width:' + stroke-width + ';fill-rule:nonzero;"/>'
+// }
+
+var config = { width: 300, height: 100, background: '#dedede', stroke: 'red' };
+var rating = function(num) {
+  var res = '';
+  for (var i=0; i<num; i++) { res += '★'; }
+  for (var i=num+1; i<5; i++) { res += '☆'; }
+  return res;
+}
+
 
 var serve = function(request, response) {
 
@@ -29,14 +49,11 @@ var serve = function(request, response) {
 
   console.log(url_parts);
 
-
   response.writeHead(200, { "Content-Type": "image/svg+xml" }); //, "Content-Disposition": "attachment; filename=" + fileName });
-  response.write('<svg xmlns="http://www.w3.org/2000/svg" version="1.1">' +
-  '<rect x="25" y="25" width="200" height="200" fill="lime" stroke-width="4" stroke="pink" />' +
-  '<circle cx="125" cy="125" r="75" fill="orange" />' +
-  '<polyline points="50,150 50,200 200,200 200,100" stroke="red" stroke-width="4" fill="none" />' +
-  '<line x1="50" y1="50" x2="200" y2="200" stroke="blue" stroke-width="4" />' +
-'</svg>');
+  response.write(`<svg xmlns="http://www.w3.org/2000/svg" height="${config.height}" width="${config.width}">
+    <rect x="0" y="0" width="${config.width}" height="${config.height}" fill="${config.background}" stroke-width="4" stroke="${config.stroke}" />
+    <text x="0" y="15" fill="yellow">${rating(Math.random() * 1000 % 5) }</text></svg>`);
+
   //response.end(file, "base64");
   response.end();
 }
