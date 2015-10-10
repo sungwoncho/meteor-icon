97a7f63e981acced4a319ade4b4b31f22880262d
Sung Won Cho <mikeswcho@gmail.com>
2015-10-11 09:40:09 +1100

    Add MDG support (to be deleted if more lines are needed)

--- client/home.js
+++ client/home.js
@@ -1,12 +1,23 @@
+function getEmbedCode(fullName, iconPath) {
+  var base = `[![Meteor Icon](${iconPath})](https://atmospherejs.com/`;
+  if (fullName.split(':')[1]) {
+    return base.concat(`${fullName.replace(/\:/, '/')}`);
+  } else {
+    return base.concat(`meteor/${fullName.split(':')[0]})`);
+  }
+}
+
+Template.home.onRendered(function () {
+  $('.embed-code').hide();
+});
+
 Template.home.events({
   'submit .demo-form': function (e, tpl) {
     e.preventDefault();
     var fullName = tpl.$('.package-name').val();
     var iconPath = Meteor.absoluteUrl(`package/${fullName}`);
     tpl.$('.demo-icon').attr('src', iconPath);
-    var embedCode = `[![Meteor Icon](${iconPath})](https://atmospherejs.com/\
-${fullName.split(':')[0]}/${fullName.split(':')[1]})`;
-    tpl.$('.embed-code').val(embedCode).removeClass('hidden');
+    tpl.$('.embed-code').val(getEmbedCode(fullName, iconPath)).show();
   },
   'click .embed-code': function (e, tpl) {
     tpl.$('.embed-code').select();
--- server/routes.js
+++ server/routes.js
@@ -3,30 +3,28 @@ function calcWidth(name) {
 }
 
 WebApp.connectHandlers.use("/package", function(request, response) {
-  if(request.url.split('/')[1] !== '') {
-    var url = `https://atmospherejs.com/a/packages/findByNames\
+  var url = `https://atmospherejs.com/a/packages/findByNames\
 ?names=${request.url.split('/')[1]}`;
-    var opts = {headers: {'Accept': 'application/json'}};
-    HTTP.get(url, opts, function(err, res) {
-      var name = '', version, pubDate, startCount, installYear;
-      var payload = res.data[0];
+  var opts = {headers: {'Accept': 'application/json'}};
+  HTTP.get(url, opts, function(err, res) {
+    var name = '', version, pubDate, startCount, installYear;
+    var payload = res.data[0];
 
-      if (res.data.length !== 0) {
-        name = payload.name;
-        version = payload.latestVersion.version;
-        pubDate = moment(payload.latestVersion.published.$date)
-                        .format('MMM Do YYYY');
-        starCount = payload.starCount.toLocaleString();
-        installYear = payload['installs-per-year'].toLocaleString();
-      }
+    if (res.data.length !== 0) {
+      name = payload.name;
+      version = payload.latestVersion.version;
+      pubDate = moment(payload.latestVersion.published.$date)
+                      .format('MMM Do YYYY');
+      starCount = payload.starCount.toLocaleString();
+      installYear = payload['installs-per-year'].toLocaleString();
+    }
 
-      SSR.compileTemplate('icon', Assets.getText('icon.svg'));
-      var width = calcWidth(name);
-      var icon = SSR.render('icon', {w: width, totalW: width+2, n: name,
-        v: version, p: pubDate, s: starCount, i: installYear});
+    SSR.compileTemplate('icon', Assets.getText('icon.svg'));
+    var width = calcWidth(name);
+    var icon = SSR.render('icon', {w: width, totalW: width+2, n: name,
+      v: version, p: pubDate, s: starCount, i: installYear});
 
-      response.writeHead(200, {"Content-Type": "image/svg+xml"});
-      response.end(icon);
-    });
-  }
+    response.writeHead(200, {"Content-Type": "image/svg+xml"});
+    response.end(icon);
+  });
 });
