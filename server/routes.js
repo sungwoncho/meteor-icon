305962b91fa65abd70b21e7008548c3ffa682278
Sung Won Cho <mikeswcho@gmail.com>
2015-10-10 15:51:33 +1100

    Move app.js to routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,28 +1,3 @@
-// if (Meteor.isClient) {
-//   // counter starts at 0
-//   Session.setDefault('counter', 0);
-//
-//   Template.hello.helpers({
-//     counter: function () {
-//       return Session.get('counter');
-//     }
-//   });
-//
-//   Template.hello.events({
-//     'click button': function () {
-//       // increment the counter when button is clicked
-//       Session.set('counter', Session.get('counter') + 1);
-//     }
-//   });
-// }
-//
-// if (Meteor.isServer) {
-//   Meteor.startup(function () {
-//     // code to run on server at startup
-//   });
-// }
-
-
 var serve = function(request, response) {
 
   var url_parts = request.url;
@@ -39,7 +14,7 @@ var serve = function(request, response) {
 '</svg>');
   //response.end(file, "base64");
   response.end();
-}
+};
 
 
 WebApp.connectHandlers.use("/image", serve);
