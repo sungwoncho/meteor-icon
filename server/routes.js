abe58241dbf96c8dc4e1a58c58aa1c3f17a6abdb
Matthew Wood <wood_matt@optusnet.com.au>
2015-10-10 17:46:12 +1100

    Correct data pulled from Atmosphere
    and applied with simple check

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,4 @@
-var config = { width: 300, height: 100, background: '#dedede', stroke: 'red' };
+var config = { width: 400, height: 100, background: '#dedede', stroke: 'red' };
 
 var serve = function(request, response) {
   var url_parts = request.url.split('/');
@@ -6,10 +6,29 @@ var serve = function(request, response) {
   var url = `https://atmospherejs.com/a/packages/findByNames?names=${url_parts[1]}`;
   var opt = {headers: {'Accept': 'application/json'}};
   HTTP.get(url, opt, function (err, res) {
+
+    var name = '';
+    var version = '';
+    var pubdate = '';
+    var starCount = '';
+    var installyear = '';
+
+    // Check to see if the response data is not empty
+    if(res.data.length != 0){
+      name = res.data[0].name;
+      version = res.data[0].latestVersion.version;
+      pubdate = moment(res.data[0].latestVersion.published.$date).format("MMM Do YY");
+      starCount = res.data[0].starCount;
+      installyear = res.data[0]['installs-per-year'];
+    }
+
     response.writeHead(200, { "Content-Type": "image/svg+xml" });
     response.write(`<svg xmlns="http://www.w3.org/2000/svg" height="${config.height}" width="${config.width}">
       <rect x="0" y="0" width="${config.width}" height="${config.height}" fill="${config.background}" stroke-width="4" stroke="${config.stroke}" />
-      <text x="20" y="20" font-family="Verdana" font-size="30">${res.data[0].name}</text>
+          <text x="20" y="20" font-family="Verdana" font-size="15">meteor add ${name}</text>
+          <text x="20" y="40" font-family="Verdana" font-size="15">${version} @ ${pubdate}</text>
+          <text x="20" y="60" font-family="Verdana" font-size="15">${starCount}</text>
+          <text x="20" y="80" font-family="Verdana" font-size="15">${installyear}</text>
       </svg>`);
     response.end();
   });
