7a930a18aa4a9843bcddd824940b7dcb6805aeb6
Sung Won Cho <mikeswcho@gmail.com>
2015-10-10 20:35:53 +1100

    Improve the appearance of the home page

--- client/home.js
+++ client/home.js
@@ -1,10 +1,11 @@
 Template.home.events({
-  'submit .demo': function (e, tpl) {
+  'submit .demo-form': function (e, tpl) {
     e.preventDefault();
-    var packageFullName = tpl.$('.package-name').val();
-    var authorName = packageFullName.split(':')[0]
-    var packageName = packageFullName.split(':')[1]
-    tpl.$('.demo-icon').attr('src', `package/${packageFullName}`);
-    tpl.$('.embed-code').attr('value', `[![Meteor Icon](package/${packageName})](https://atmospherejs.com/${authorName}/${packageName}})`).removeClass('hidden');
+    var fullName = tpl.$('.package-name').val();
+    var authorName = fullName.split(':')[0];
+    var packageName = fullName.split(':')[1];
+    var iconPath = Meteor.absoluteUrl(`package/${fullName}`);
+    tpl.$('.demo-icon').attr('src', `package/${fullName}`);
+    tpl.$('.embed-code').val(`[![Meteor Icon](${iconPath})](https://atmospherejs.com/${authorName}/${packageName}})`).removeClass('hidden');
   }
 });
