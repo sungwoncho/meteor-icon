c13cc65c7ca3b2e2982b57d220ca5ce7ec5d0afc
Sung Won Cho <mikeswcho@gmail.com>
2015-10-10 20:06:21 +1100

    Link to atmosphere

--- client/home.js
+++ client/home.js
@@ -1,8 +1,10 @@
 Template.home.events({
   'submit .demo': function (e, tpl) {
     e.preventDefault();
-    var packageName = tpl.$('.package-name').val();
-    tpl.$('.demo-icon').attr('src', `package/${packageName}`);
-    tpl.$('.embed-code').attr('value', `[![Build Status](package/${packageName})](package/${packageName})`).removeClass('hidden');
+    var packageFullName = tpl.$('.package-name').val();
+    var authorName = packageFullName.split(':')[0]
+    var packageName = packageFullName.split(':')[1]
+    tpl.$('.demo-icon').attr('src', `package/${packageFullName}`);
+    tpl.$('.embed-code').attr('value', `[![Meteor Icon](package/${packageName})](https://atmospherejs.com/${authorName}/${packageName}})`).removeClass('hidden');
   }
 });
