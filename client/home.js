df0874c597603c075098ec602deeea9f6c00e82f
Sung Won Cho <mikeswcho@gmail.com>
2015-10-10 22:54:40 +1100

    Select the embed code on click

--- client/home.js
+++ client/home.js
@@ -6,5 +6,8 @@ Template.home.events({
     tpl.$('.demo-icon').attr('src', iconPath);
     var embedCode = `[![Meteor Icon](${iconPath})](https://atmospherejs.com/${fullName.split(':')[0]}/${fullName.split(':')[1]}})`
     tpl.$('.embed-code').val(embedCode).removeClass('hidden');
+  },
+  'click .embed-code': function (e, tpl) {
+    tpl.$('.embed-code').select();
   }
 });
