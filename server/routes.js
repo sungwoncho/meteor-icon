e430e77e9a78116fd6c22c43c95c68ffcff2be5d
Sung Won Cho <mikeswcho@gmail.com>
2015-10-10 16:42:50 +1100

    Make API call to atmosphere and display the package name in svg

--- server/routes.js
+++ server/routes.js
@@ -1,24 +1,18 @@
 var config = { width: 300, height: 100, background: '#dedede', stroke: 'red' };
-var rating = function(num) {
-  var res = '';
-  for (var i=0; i<num; i++) { res += '★'; }
-  for (var i=num+1; i<5; i++) { res += '☆'; }
-  return res;
-}
 
 var serve = function(request, response) {
-
   var url_parts = request.url;
 
-  console.log(url_parts);
-
-  response.writeHead(200, { "Content-Type": "image/svg+xml" }); //, "Content-Disposition": "attachment; filename=" + fileName });
-  response.write(`<svg xmlns="http://www.w3.org/2000/svg" height="${config.height}" width="${config.width}">
-    <rect x="0" y="0" width="${config.width}" height="${config.height}" fill="${config.background}" stroke-width="4" stroke="${config.stroke}" />
-    <text x="0" y="15" fill="yellow">${rating(Math.random() * 1000 % 5) }</text></svg>`);
-
-  //response.end(file, "base64");
-  response.end();
+  var url = `https://atmospherejs.com/a/packages/findByNames?names=iron:router`;
+  var opt = {headers: {'Accept': 'application/json'}};
+  HTTP.get(url, opt, function (err, res) {
+    response.writeHead(200, { "Content-Type": "image/svg+xml" });
+    response.write(`<svg xmlns="http://www.w3.org/2000/svg" height="${config.height}" width="${config.width}">
+      <rect x="0" y="0" width="${config.width}" height="${config.height}" fill="${config.background}" stroke-width="4" stroke="${config.stroke}" />
+      <text x="20" y="20" font-family="Verdana" font-size="30">${res.data[0].name}</text>
+      </svg>`);
+    response.end();
+  })
 };
 
 
