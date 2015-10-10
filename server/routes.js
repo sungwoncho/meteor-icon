7ee36272320fcfa8bc678ef10a6da0ff22cc0c45
Matthew Wood <wood_matt@optusnet.com.au>
2015-10-11 01:11:37 +1100

    Changed process for localString added Check to url

--- server/routes.js
+++ server/routes.js
@@ -5,25 +5,27 @@ function calcWidth(name) {
 }
 
 WebApp.connectHandlers.use("/package", function(request, response) {
+  if(request.url.split('/')[1] != ''){
   var url = `https://atmospherejs.com/a/packages/findByNames\
 ?names=${request.url.split('/')[1]}`;
   HTTP.get(url, {headers: {'Accept': 'application/json'}}, function(err, res) {
-    var name = '';
+    var name,version,pubdate,starCount,installyear = '';
     if (res.data.length != 0) {
       name = res.data[0].name;
       version = res.data[0].latestVersion.version;
       pubdate = moment(res.data[0].latestVersion.published.$date).format(f);
-      starCount = res.data[0].starCount;
-      installyear = res.data[0]['installs-per-year'];
+      starCount = res.data[0].starCount.toLocaleString();
+      installyear = res.data[0]['installs-per-year'].toLocaleString();
     }
 
     SSR.compileTemplate('icon', Assets.getText('icon.svg'));
     var width = calcWidth(name);
     var params = {w: width, totalW: width+2, n: name, v: version, p: pubdate,
-      s: starCount.toLocaleString(), i: installyear.toLocaleString()};
+      s: starCount, i: installyear};
     var icon = SSR.render('icon', params);
 
     response.writeHead(200, {"Content-Type": "image/svg+xml"});
     response.end(icon);
   });
+}
 });
