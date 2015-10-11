868881714fe732a720b1d2a694a10c37dff0b67e
Tomas Trescak <tomi.trescak@gmail.com>
2015-10-11 12:18:47 +1100

    Rating

--- server/routes.js
+++ server/routes.js
@@ -18,12 +18,29 @@ WebApp.connectHandlers.use("/package", function(request, response) {
       installYear = pl['installs-per-year'] || 0;
     }
 
-    SSR.compileTemplate('icon', Assets.getText('icon.svg'));
     var width = calcWidth(name);
-    var icon = SSR.render('icon', {w: width, totalW: width+2, n: name,
-      v: version, p: pubDate, s: starCount, i: installYear});
+    var c = DDP.connect('https://atmospherejs.com/');
+    var cl = new Meteor.Collection('scores', c);
+    c.subscribe('package/dailyScores', request.url.split('/')[1], function(er, m) {
+      var min = 100000, max = 0, scores= [width + "," + 80, "0,80"], i=0;
+      cl.find().forEach(function(data){
+        data.score > max ? (max = data.score) : (data.score < min ? min = data.score : min = min);
+      });
+      cl.find().forEach(function(data){
+        scores.push((i++*(width/5)) + "," + ((30 - 30 * ((data.score-min) / (max-min))) + 23));
+      });
+      console.log(scores.length)
 
-    response.writeHead(200, {"Content-Type": "image/svg+xml"});
-    response.end(icon);
+      SSR.compileTemplate('icon', Assets.getText('icon.svg'));
+
+      var icon = SSR.render('icon', {w: width, totalW: width+2, n: name,
+        v: version, p: pubDate, s: starCount, i: installYear, scores: scores,
+        ls: (width - 20), lsv: Math.ceil(max)});
+
+      response.writeHead(200, {"Content-Type": "image/svg+xml"});
+      response.end(icon);
+    });
+
+
   });
 });
