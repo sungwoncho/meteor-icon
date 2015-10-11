18f499d904e32040f10cdf01707b595060282ed7
Sung Won Cho <mikeswcho@gmail.com>
2015-10-11 13:16:19 +1100

    Fix bug where Not Found is not displayed

--- server/routes.js
+++ server/routes.js
@@ -3,38 +3,39 @@ WebApp.connectHandlers.use("/package", function (request, response) {
 ?names=${request.url.split('/')[1]}`;
   var opts = {headers: {'Accept': 'application/json'}};
   HTTP.get(url, opts, function(err, res) {
-    var name = '', pl = res.data[0], version, pubDate, starCount, installYear;
+    var name = '', pl = res.data[0], version, pubDate, starCount, installYear, icon;
     if (res.data.length !== 0) {
-      name = pl.name;
-      version = pl.latestVersion.version;
+      name = pl.name; version = pl.latestVersion.version;
       pubDate = moment(pl.latestVersion.published.$date).format('MMM Do YYYY');
       starCount = pl.starCount || 0;
       installYear = pl['installs-per-year'] || 0;
     }
-
     var width = 225 + name.length * 6.305555555555555;
+    var params = {w: width, totalW: width+2, n: name,
+      v: version, p: pubDate, s: starCount, i: installYear, ls: (width - 75)};
     var c = DDP.connect('https://atmospherejs.com/');
     var cl = new Meteor.Collection('scores', c);
-    c.subscribe('package/dailyScores', name, function(er, m) {
-      var min = 100000, max = 0, scores= [width + "," + 80, "0,80"], i=0;
-      cl.find().forEach(function(data){
-        data.score > max ? (max = Math.ceil(data.score))
-        : (data.score < min ? min = data.score : min = min);
+    response.writeHead(200, {"Content-Type": "image/svg+xml"});
+    SSR.compileTemplate('icon', Assets.getText('icon.svg'));
+    if (name) {
+      c.subscribe('package/dailyScores', name, function(er, m) {
+        var min = 100000, max = 0, scores= [width + "," + 80, "0,80"], i=0;
+        cl.find().forEach(function(data){
+          data.score > max ? (max = Math.ceil(data.score))
+          : (data.score < min ? min = data.score : min = min);
+        });
+        cl.find().forEach(function(data){
+          scores.push((i++*(width/5)) + "," +
+           ((30 - 30 * ((data.score-min) / (max-min))) + 23));
+        });
+        _.extend(params, {scores: scores, lsv: max, st: max > 4 ? "★★★★★"
+        : ("★★★★★".substring(0, max) + "☆☆☆☆".substring(0, 5-max))});
+        icon = SSR.render('icon', params);
+        response.end(icon);
       });
-      cl.find().forEach(function(data){
-        scores.push((i++*(width/5)) + "," +
-         ((30 - 30 * ((data.score-min) / (max-min))) + 23));
-      });
-
-      SSR.compileTemplate('icon', Assets.getText('icon.svg'));
-      var icon = SSR.render('icon', {w: width, totalW: width+2, n: name,
-        v: version, p: pubDate, s: starCount, i: installYear, scores: scores,
-        ls: (width - 75), lsv: max,
-        st: max > 4 ? "★★★★★"
-        : ("★★★★★".substring(0, max) + "☆☆☆☆".substring(0, 5-max)) });
-
-      response.writeHead(200, {"Content-Type": "image/svg+xml"});
+    } else {
+      icon = SSR.render('icon', params);
       response.end(icon);
-    });
+    }
   });
 });
