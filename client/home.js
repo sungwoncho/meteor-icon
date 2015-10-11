bea0afc8f376920d46ea69c02b90fc045d8b6875
Sung Won Cho <mikeswcho@gmail.com>
2015-10-11 11:10:37 +1100

    Fix broken icon

--- client/home.js
+++ client/home.js
@@ -1,12 +1,3 @@
-function getEmbedCode(fullName, iconPath) {
-  var base = `[![Meteor Icon](${iconPath})](https://atmospherejs.com/`;
-  if (fullName.split(':')[1]) {
-    return base.concat(`${fullName.replace(/\:/, '/')}`);
-  } else {
-    return base.concat(`meteor/${fullName.split(':')[0]})`);
-  }
-}
-
 Template.home.onRendered(function () {
   $('.embed-code').hide();
 });
@@ -17,7 +8,8 @@ Template.home.events({
     var fullName = tpl.$('.package-name').val();
     var iconPath = Meteor.absoluteUrl(`package/${fullName}`);
     tpl.$('.demo-icon').attr('src', iconPath);
-    tpl.$('.embed-code').val(getEmbedCode(fullName, iconPath)).show();
+    tpl.$('.embed-code').val(`[![Meteor Icon](${iconPath})](https://\
+atmospherejs.com/${fullName.replace(/\:/, '/')}`).show();
   },
   'click .embed-code': function (e, tpl) {
     tpl.$('.embed-code').select();
--- server/routes.js
+++ server/routes.js
@@ -8,14 +8,14 @@ WebApp.connectHandlers.use("/package", function(request, response) {
   var opts = {headers: {'Accept': 'application/json'}};
   HTTP.get(url, opts, function(err, res) {
     var name = '', version, pubDate, starCount, installYear;
-    var payload = res.data[0];
+    var pl = res.data[0];
 
-    if (! res.data.length) {
-      name = payload.name;
-      version = payload.latestVersion.version;
-      pubDate = moment(payload.latestVersion.published.$date).format('MMM Do YYYY');
-      starCount = payload.starCount || 0;
-      installYear = payload['installs-per-year'] || 0;
+    if (res.data.length !== 0) {
+      name = pl.name;
+      version = pl.latestVersion.version;
+      pubDate = moment(pl.latestVersion.published.$date).format('MMM Do YYYY');
+      starCount = pl.starCount || 0;
+      installYear = pl['installs-per-year'] || 0;
     }
 
     SSR.compileTemplate('icon', Assets.getText('icon.svg'));
